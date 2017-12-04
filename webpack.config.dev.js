47f22e6ff573de3708771d23e06bf68ad06a3657
ruiting.xing <ruiting.xing@alibaba-inc.com>
2017-12-05 00:46:36 +0800

    + yarn start

---
+++ webpack.config.dev.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: {
+    devServer: 'webpack-dev-server/client?http://0.0.0.0:3000',
+    index: './src/index.tsx',
+  },
+  output: {
+    path: __dirname + '/built',
+    publicPath: '/built',
+    filename: '[name].js',
+    chunkFilename: '[name]-[chunkhash].js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [{
+      test: /\.tsx?$/,
+      loader: 'ts-loader'
+    }]
+  },
+  plugins: [
+    new webpack.WatchIgnorePlugin([
+      /\.js$/,
+      /\.d\.ts$/
+    ])
+  ]
+}
